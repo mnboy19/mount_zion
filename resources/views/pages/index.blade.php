0b7c8e64bbd1eb2441f989e489919dbf226cf877
mnboy19 <jchrisboy0@gmail.com>
2021-08-16 18:04:34 -0500

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Welcome to Laravel for beginers</h1>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui labore laborum, nemo vitae rem repellendus tempora! Corrupti nam quam nobis nesciunt debitis optio sed, ex eius veniam alias aliquid fuga!</p>
+@endsection
