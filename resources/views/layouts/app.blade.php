306fb0ab2a80efa86dc00c2bf67e70bf57582459
mnboy19 <jchrisboy0@gmail.com>
2021-08-17 15:11:27 -0500

    fifth commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,6 +82,29 @@
             </div>
         </nav>
 
+        <div class="primary_nav_wrap navbar navbar-expand-md navbar-inverse shadow-sm">
+            <ul class="navbar-nav container">
+                <li class="nav-item">
+                    <a  href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a  href="/office">Offices</a>
+                </li>
+                <li class="nav-item">
+                    <a  href="#">Events & Activities</a>
+                </li>
+                <li class="nav-item">
+                    <a  href="#">Services & Multimedia</a>
+                </li>
+                <li class="nav-item">
+                    <a  href="#">About US</a>
+                </li>
+                <li class="nav-item">
+                    <a  href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+
         <main class="py-4">
             @yield('content')
         </main>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,26 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <nav class="primary_nav_wrap navbar navbar-expand-md navbar-inverse shadow-sm">
-        <ul class="navbar-nav container">
-            <li class="nav-item">
-                <a  href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a  href="#">Offices</a>
-            </li>
-            <li class="nav-item">
-                <a  href="#">Events & Activities</a>
-            </li>
-            <li class="nav-item">
-                <a  href="#">Services & Multimedia</a>
-            </li>
-            <li class="nav-item">
-                <a  href="#">About US</a>
-            </li>
-            <li class="nav-item">
-                <a  href="#">Contact Us</a>
-            </li>
-        </ul>
-    </nav>
+    <div>
+        <img class="sun" src="{{ asset('img/grain.jpg') }}" alt="">
+    </div>
+
+    
 @endsection
